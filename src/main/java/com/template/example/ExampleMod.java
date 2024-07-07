6e3e4aa17a660ac6d9d7f2f4e5354192502c18c1
PinkGoosik <MCGycuk@yandex.ru>
2024-07-07 20:21:39 +0500

    fix B)

--- src/main/java/com/template/example/ExampleMod.java
+++ src/main/java/com/template/example/ExampleMod.java
@@ -21,6 +21,6 @@ public class ExampleMod implements ModInitializer {
 	}
 
 	public static Identifier id(String path) {
-		return new Identifier(MOD_ID, path);
+		return Identifier.of(MOD_ID, path);
 	}
 }
